c48297d410cebd79bf50ac73a56b18be53d5e595
h <cqdev@tuta.com>
2024-06-30 13:53:01 +1000

    alazy options

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -23,6 +23,16 @@ require("lazy").setup({
       },
     },
     -- import/override with your plugins
+    { import = "lazyvim.plugins.extras.lang.git" },
+    { import = "lazyvim.plugins.extras.lang.markdown" },
+    { import = "lazyvim.plugins.extras.lang.python" },
+    { import = "lazyvim.plugins.extras.lang.tailwind" },
+    { import = "lazyvim.plugins.extras.lang.typescript" },
+    { import = "lazyvim.plugins.extras.linting.eslint" },
+    { import = "lazyvim.plugins.extras.lang.json" },
+    { import = "lazyvim.plugins.extras.lsp.neoconf" },
+    { import = "lazyvim.plugins.extras.ui.mini-animate" },
+    { import = "lazyvim.plugins.extras.util.mini-hipatterns" },
     { import = "plugins" },
   },
   defaults = {
